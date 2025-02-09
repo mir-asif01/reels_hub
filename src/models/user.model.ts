729964bbcf20127512be5741c57953988688b412
mir-asif01 <asifbsb2002@gmail.com>
2025-02-09 23:19:31 +0600

    added user and video model

---
+++ src/models/user.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+interface IUser {
+  _id?: mongoose.Types.ObjectId;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    await bcrypt.hash(this.password, 10);
+  }
+});
+
+export const User = models?.User || model<IUser>("user", userSchema);
---
+++ src/models/video.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+interface IVideo {
+  _id?: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  videoUrl: string;
+  controls?: boolean;
+  tranformations?: {
+    height: number;
+    width: number;
+    quality: number;
+  };
+}
+
+const VIDEO_DIMENSION = {
+  height: 1920,
+  width: 1080,
+} as const;
+
+const videoSchema = new Schema<IVideo>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    videoUrl: { type: String, required: true },
+    controls: { type: Boolean, default: true },
+    tranformations: {
+      height: { type: Number, default: VIDEO_DIMENSION.height },
+      width: { type: Number, default: VIDEO_DIMENSION.width },
+      quality: { type: Number, min: 1, max: 1000 },
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = models?.Video || model<IVideo>("video", videoSchema);
